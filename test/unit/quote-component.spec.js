44d727fce245f08192dfb7040424a2a3a6e69453
rodzzlessa <rodzzlessa@gmail.com>
2016-04-29 11:27:33 -0400

    update tests

--- test/unit/quote-component.spec.js
+++ test/unit/quote-component.spec.js
@@ -1,15 +1,23 @@
 var Vue = require('vue')
 var Quote = require('../../src/components/Quote.vue')
+const QuoteServiceInjector = require('!!vue?inject!../../src/components/Quote.vue')
 
-describe('Quote.vue', function () {
+const QuoteWithMocks = QuoteServiceInjector({
+  // mock it
+  '../services/quote': {
+    quote: 'Hello from a mocked service!'
+  }
+})
+
+describe('Quote.vue', function() {
 
   // asserting JavaScript options
-  it('should have correct message', function () {
+  it('should have correct message', function() {
     expect(Quote.data().quote).toBe('')
-  })
+  });
 
   // asserting rendered result by actually rendering the component
-  it('should render correct message', function () {
+  it('should render correct message', function() {
     var vm = new Vue({
       template: '<div><test></test></div>',
       components: {
@@ -17,5 +25,5 @@ describe('Quote.vue', function () {
       }
     }).$mount()
     expect(vm.$el.querySelector('h2').textContent).toBe('Random Quote')
-  })
+  });
 })
